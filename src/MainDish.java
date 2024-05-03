ac1554d3e29f77da9417ff60a6adcdd922008380
_basmalahmedd <ahmedbasmala74@gmail.com>
2024-05-04 00:51:32 +0300

    Create MainDish class

---
+++ src/MainDish.java
@@ -0,0 +1,19 @@
+public class MainDish extends MenuItem {
+    private int spicinessLevel;
+
+    public MainDish(String name, int price, int stockQuantity,String description, int spicinessLevel) {
+        super(name, price, stockQuantity,description);
+        setSpicinessLevel(spicinessLevel);
+    }
+
+    public int getSpicinessLevel() {
+        return spicinessLevel;
+    }
+
+    public void setSpicinessLevel(int spicinessLevel) {
+        if (spicinessLevel < 0 || spicinessLevel > 3) {
+            throw new IllegalArgumentException("Spiciness level must be between zero and three (inclusive)");
+        }
+        this.spicinessLevel = spicinessLevel;
+    }
+}
