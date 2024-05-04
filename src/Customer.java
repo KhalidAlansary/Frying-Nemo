1880575d8f2446f7d4b7a642a6bea36220ecaf56
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-04 22:04:52 +0300

    Customer class updates

--- src/Customer.java
+++ src/Customer.java
@@ -1,6 +1,10 @@
-public class Customer {
+public class Customer extends Person {
     private int points;
     public final double pointsMonetaryValue = 0.01;
+
+    public Customer(String email, String firstName, String lastName) {
+        super(email, firstName, lastName);
+    }
 
     public int getPoints() {
         return points;
@@ -11,19 +15,14 @@ public class Customer {
     }
 
     public void redeem_points(int pointsToBeRedeemed) {
-        try {
-            if (pointsToBeRedeemed >= points) {
-                this.points -= pointsToBeRedeemed;
-                double MonetaryValue = points * pointsMonetaryValue;
-                System.out.println("points redeemed with value " + MonetaryValue);
-                System.out.println("Points left = " + points);
-            }
-            else {
-                throw new IllegalArgumentException();
-            }
+        if (pointsToBeRedeemed <= points) {
+            this.points -= pointsToBeRedeemed;
+            double MonetaryValue = points * pointsMonetaryValue;
+            System.out.println("points redeemed with value " + MonetaryValue);
+            System.out.println("Points left = " + points);
         }
-        catch (IllegalArgumentException e) {
-            System.out.println("INSUFFICIENT POINTS CAN'T COMPLETE ACTION");
+        else {
+            throw new IllegalArgumentException();
         }
 
     }
