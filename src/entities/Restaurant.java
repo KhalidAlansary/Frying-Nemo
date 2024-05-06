451a8f113b13427326e08afbdbf0e65d4f5d1d84
salma <salmahamed912@gmail.com>
2024-05-06 22:38:06 +0300

    feat: add ArrayList of Dessert to attributes

--- src/entities/Restaurant.java
+++ src/entities/Restaurant.java
@@ -6,6 +6,8 @@ public class Restaurant {
     private int noOfTables;
     private int noOfStaff;
     public static ArrayList<StaffMember> staffMembers = new ArrayList<StaffMember>();
+    public static ArrayList<MainDish> mainDishes = new ArrayList<MainDish>();
+    public static ArrayList<Dessert> desserts = new ArrayList<Dessert>();
 
     public void setNoOfStaff(int noOfStaff) {
         this.noOfStaff = noOfStaff;
