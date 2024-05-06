274933194d0c7075be95073e088ca4146ee322d1
KhalidAlansary <khaled.ansary2003@outlook.com>
2024-05-07 01:08:36 +0300

    feat: initialized some customers

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,5 +1,7 @@
 package controllers;
 
+import entities.Customer;
+import entities.Restaurant;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -12,6 +14,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        initialize();
+
         Main.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("/resources/Main.fxml"));
         primaryStage.setTitle("Frying Nemo Restaurant");
@@ -23,5 +27,11 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    public static void initialize() {
+        Restaurant.customers.add(new Customer("harry@hogwarts.edu", "Harry", "Potter"));
+        Restaurant.customers.add(new Customer("ron@hogwarts.edu", "Ron", "Weasley"));
+        Restaurant.customers.add(new Customer("hermione@hogwarts.edu", "Hermione", "Granger"));
     }
 }
