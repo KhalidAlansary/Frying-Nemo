ec1b1f97a0edb04142ff342c7047e2d69d51a60d
KhalidAlansary <khaled.ansary2003@outlook.com>
2024-05-06 17:21:44 +0300

    chore: set controller as MainController and from it call the switch scene method from SceneController

--- src/controllers/MainController.java
+++ src/controllers/MainController.java
@@ -1,5 +1,12 @@
 package controllers;
 
-public class MainController {
+import java.io.IOException;
 
+import javafx.event.ActionEvent;
+
+public class MainController {
+    public void switchToNewStaffMemberScene(ActionEvent event) throws IOException {
+        SceneController sceneController = new SceneController();
+        sceneController.switchToNewStaffMemberScene(event);
+    }
 }
