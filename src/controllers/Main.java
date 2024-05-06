bb96d8d0a6caacc09297dd81f20393da1accab86
_basmalahmedd <ahmedbasmala74@gmail.com>
2024-05-07 00:10:39 +0300

    Edited main class

--- src/controllers/Main.java
+++ src/controllers/Main.java
@@ -1,18 +1,24 @@
 package controllers;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+
+import java.awt.*;
 
 public class Main extends Application {
     public static Stage primaryStage; // **Declare static Stage**
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Main.primaryStage = primaryStage;
         Parent root = FXMLLoader.load(getClass().getResource("/resources/Main.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("Frying Nemo Restaurant");
+        Image image = new Image("/resources/nemo.png");
+        primaryStage.getIcons().add(image);
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
