9ed9fca38dc0acbabbbcffcca59f9eb81452ac60
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-06 12:55:39 +0300

    Restaurant and Table classes

--- src/Table.java
+++ src/Table.java
@@ -1,3 +1,31 @@
 public class Table {
+private static int noOfTablesInUse;
+private static int freeTables;
+private int  tableNo;
+private int capacity;
+private boolean reserved;
+
+String email;
+String firstName;
+String lastName;
+
+Customer reservingCustomer = new Customer(email,firstName,lastName);
+
+void setNoOfTables (int noOfTables){
+    boolean positiveNo = true;
+    while(positiveNo){
+        if (noOfTables >= 0) {
+            this.noOfTablesInUse = noOfTables;
+            positiveNo = false;
+        }
+        else{
+            throw new IllegalArgumentException("not a valid No.");
+        }
+
+    }
+}
+
+
+
 
 }
--- src/module-info.java
+++
@@ -1,7 +0,0 @@
-module Frying.Nemo {
-    requires javafx.fxml;
-    requires javafx.controls;
-
-    opens Main;
-
-}
