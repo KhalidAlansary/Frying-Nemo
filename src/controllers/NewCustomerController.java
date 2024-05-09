0fde41bb7614894f9fcfa12be7c09a9d013578b6
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-10 01:03:55 +0300

    customer controller to add new customers

--- src/controllers/NewCustomerController.java
+++ src/controllers/NewCustomerController.java
@@ -1,4 +1,43 @@
 package controllers;
 
-public class NewCustomer {
+import entities.Customer;
+import entities.Restaurant;
+import entities.Customer;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
+public class NewCustomerController {
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField firstNameTextField;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    void addCustomer(ActionEvent event) {
+        try {
+            Customer customer  = new Customer(emailTextField.getText(), firstNameTextField.getText(),
+                    lastNameTextField.getText());
+            Restaurant.customers.add(customer);
+            SceneController sceneController = new SceneController();
+            sceneController.switchToMainScene(event, "Customer added successfully!");
+        } catch (IllegalArgumentException | IOException e) {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText(e.getMessage());
+            alert.show();
+        }
+    }
+
 }
