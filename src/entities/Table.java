a9971a1f19bb6003f68a092e216c70479fef3d21
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-10 01:50:04 +0300

    Table class

--- src/entities/Table.java
+++ src/entities/Table.java
@@ -1,29 +1,22 @@
 package entities;
 
+import javafx.scene.control.Tab;
+
 public class Table {
+
+    private int capacity;
+    private final int tableNo;
+    private static int noOfTables=0;
     private static int noOfTablesInUse;
     private static int freeTables;
-    private int tableNo;
-    private int capacity;
     private boolean reserved;
 
-    String email;
-    String firstName;
-    String lastName;
-
-    Customer reservingCustomer = new Customer(email, firstName, lastName);
-
-    void setNoOfTables(int noOfTables) {
-        boolean positiveNo = true;
-        while (positiveNo) {
-            if (noOfTables >= 0) {
-                this.noOfTablesInUse = noOfTables;
-                positiveNo = false;
-            } else {
-                throw new IllegalArgumentException("not a valid No.");
-            }
-
-        }
+    public Table(int capacity){
+        this.capacity=capacity;
+        this.tableNo=noOfTables+1;
+        noOfTables++;
     }
+
+
 
 }
