b2d45eaa2ef08120361452ecafd5cf900f8925b0
fkamalo <167641692+fkamalo@users.noreply.github.com>
2024-05-11 23:24:47 +0300

    get total & make order buttons

--- src/controllers/NewOrderController.java
+++ src/controllers/NewOrderController.java
@@ -6,8 +6,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -18,14 +20,27 @@ public class NewOrderController implements Initializable {
     ChoiceBox<MainDish> mainDishChoiceBox;
     @FXML
     ChoiceBox<Dessert> dessertChoiceBox ;
-    public void addOrder(ActionEvent event) {
-
-    }
+    @FXML
+    private Label total;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         customerChoiceBox.setItems(FXCollections.observableArrayList(Restaurant.customers));
         mainDishChoiceBox.setItems(FXCollections.observableArrayList(Restaurant.mainDishes));
         dessertChoiceBox.setItems(FXCollections.observableArrayList(Restaurant.desserts));
+    }
+
+    @FXML
+    void getTotal(ActionEvent event) {
+        int Total = 0;
+        Total+=mainDishChoiceBox.getValue().getPrice();
+        Total+=dessertChoiceBox.getValue().getPrice();
+        total.setText(String.valueOf(Total));
+    }
+
+    @FXML
+    void makeOrder(ActionEvent event) throws IOException {
+        SceneController sceneController = new SceneController();
+        sceneController.switchToMainScene(event, "Order made successfully");
     }
 }
