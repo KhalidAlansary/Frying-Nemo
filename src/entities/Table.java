0b5f34f024a6e2e67d62f47b5638160b2c3cfb70
KhalidAlansary <khaled.ansary2003@outlook.com>
2024-05-13 13:00:24 +0300

    style: apply code formatter

--- src/entities/Table.java
+++ src/entities/Table.java
@@ -4,35 +4,31 @@ public class Table {
 
     private int capacity;
     private final int tableNo;
-    private static int noOfTables=0;
+    private static int noOfTables = 0;
     private boolean reserved = false;
     public Customer reservingCustomer;
 
-    public Table(int capacity){
-        this.capacity=capacity;
-        this.tableNo=noOfTables+1;
+    public Table(int capacity) {
+        this.capacity = capacity;
+        this.tableNo = noOfTables + 1;
         noOfTables++;
     }
 
-    public boolean getavailability(){
+    public boolean getavailability() {
         return reserved;
     }
 
-    public void freeATable(){
-       reserved = false;
+    public void freeATable() {
+        reserved = false;
     }
-    public void reserveATable(){
+
+    public void reserveATable() {
         reserved = true;
     }
 
-
-
     @Override
     public String toString() {
-        return("tableNo: "+tableNo+", Capacity: " + capacity) ;
+        return ("tableNo: " + tableNo + ", Capacity: " + capacity);
     }
-
-
-
 
 }
